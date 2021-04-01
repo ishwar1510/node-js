a008c2d101a8a631288ee6ec2695a4150e2ade5f
ishwar1510 <ijh.hkr9@gmail>
2021-04-01 10:55:17 +0530

    os modules

--- WriteFile.js
+++ WriteFile.js
@@ -1,4 +1,14 @@
-var fs = require('fs')
+// var fs = require('fs')
 
-fs.writeFile('Ishwar.js','console.log("done")',((err)=>{
-   console.log("my data saved here:done")}))
+// fs.writeFile('Ishwar.js','console.log("done")',((err)=>{
+//    console.log("my data saved here:done")}))
+
+   // const path = require('path')
+    
+   // var pathObj = path.parse(__filename);
+
+   // console.log(pathObj);
+
+   const path = require('path')
+
+   console.log(path.join("users/admin/nodejs","writeFile.js"));
